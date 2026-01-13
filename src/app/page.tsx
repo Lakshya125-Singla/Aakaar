941088d74a68885ed9d969d82e3ccadd6011d15f
LAKSHYA SINGLA <lakshyasingla125@gmail.com>
2026-01-13 16:52:05 +0530

    File added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,17 @@ import Trust from "./Component/Trust";
 import White from "./Component/White";
 import Science from "./Component/Science"; 
 import Unseen from "./Component/Unseen";
+import Systems from './Components/Systems';
+import Uncompromise from './Components/Uncompromise';
+import Clinical from './Components/Clinical';
 
 
 export default function Home() {
   return (
    <>
-   
+   <Systems/>
+   <Uncompromise/>
+   <Clinical/>
    <Science/>
     <Unseen/>
    <White/>
